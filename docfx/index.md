2626d3ccc3cf0029b6d073bf3d8d2fe7d2d0e6a8
martycook <martinc@valornet.com>
2021-05-29 09:22:28 -0500

    initial commit

---
+++ docfx/index.md
@@ -0,0 +1,12 @@
+<img src="~/images/codegator-334x158.png" />
+
+# Introduction
+
+CG.Caching.Redis.dll contains Redis extensions used by the CG.Caching package.
+
+
+
+
+
+
+
---
+++ src/CG.Caching.Redis/ServiceCollectionExtensions.cs
@@ -0,0 +1,66 @@
+﻿using CG.Validations;
+using Microsoft.Extensions.Caching.Redis;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CG.Caching
+{
+    /// <summary>
+    /// This class contains extension methods related to the <see cref="IServiceCollection"/>
+    /// type, for registering caching extensions.
+    /// </summary>
+    public static partial class ServiceCollectionExtensions
+    {
+        // *******************************************************************
+        // Public methods.
+        // *******************************************************************
+
+        #region Public methods
+
+        /// <summary>
+        /// This method registers a Redis distributed caching service with the 
+        /// specified service collection.
+        /// </summary>
+        /// <param name="serviceCollection">The service collection to use for
+        /// the operation.</param>
+        /// <param name="configuration">The configuration to use for the operation.</param>
+        /// <param name="serviceLifetime">The service lifetime to use for the operation.</param>
+        /// <returns>The value of the <paramref name="serviceCollection"/> 
+        /// parameter, for chaining calls together.</returns>
+        public static IServiceCollection AddRedisStrategies(
+            this IServiceCollection serviceCollection,
+            IConfiguration configuration,
+            ServiceLifetime serviceLifetime = ServiceLifetime.Scoped
+            )
+        {
+            // Validate the parameters before attempting to use them.
+            Guard.Instance().ThrowIfNull(serviceCollection, nameof(serviceCollection))
+                .ThrowIfNull(configuration, nameof(configuration));
+
+            // Register the options.
+            serviceCollection.ConfigureOptions<RedisCacheOptions>(
+                configuration,
+                out var options
+                );
+
+            // Register the ASP.NET cache service.
+            serviceCollection.AddDistributedRedisCache(configure =>
+            {
+                configure = options;
+            });
+
+            // Register the wrapper service.
+            serviceCollection.Add<ICache, Cache>(serviceLifetime);
+
+            // Return the service collection.
+            return serviceCollection;
+        }
+
+        #endregion
+    }
+}
